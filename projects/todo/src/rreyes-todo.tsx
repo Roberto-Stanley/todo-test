5c47eafca8076a8f05c19d803ffc55d81204f3fb
Roberto Reyes <robertostanleyreyes@gmail.com>
2024-04-22 03:03:21 -0600

    refactoring design

--- projects/todo/src/rreyes-todo.tsx
+++ projects/todo/src/rreyes-todo.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import singleSpaReact from "single-spa-react";
 import Root from "./root.component";
+import "./assets/app.css";
 
 const lifecycles = singleSpaReact({
   React,
